6629ca9ca843c282244ecfd2598db7f9ad148723
Alexander Podshivalov <a_podshivalov@mail.ru>
2021-10-29 01:32:07 +0300

    examples-miem/blinky2021: Added conditional compilation of different versions of the example

--- examples-miem/blinky2021/main.c
+++ examples-miem/blinky2021/main.c
@@ -10,8 +10,9 @@ void delay_ms(const uint16_t ms) {
 }
 
 int main(void) {
+    /* Example 1 - direct access to registers */
+#if 0
     periph_clk_en(AHB, RCC_AHBENR_GPIOAEN);
-
     *((uint32_t*) 0x40020000) = (1 << (5*2));
     while(1) {
         delay_ms(500);
@@ -19,7 +20,11 @@ int main(void) {
         delay_ms(500);
         *((uint32_t*) 0x40020018) = (1 << (5 + 16));
     }
+#endif
 
+    /* Example 2 - access to registers with CMSIS */
+#if 0
+    periph_clk_en(AHB, RCC_AHBENR_GPIOAEN);
     uint32_t tmpreg;
     tmpreg = GPIOA->MODER;
     tmpreg &= ~GPIO_MODER_MODER5;
@@ -31,7 +36,10 @@ int main(void) {
         delay_ms(500);
         GPIOA->BSRR = GPIO_BSRR_BR_5;
     }
-    
+#endif
+
+    /* Example 3 - RIOT's hardware abstraction */
+#if 1
     gpio_init(GPIO_PIN(PORT_A, 5), GPIO_OUT);
     while(1) {
         delay_ms(500);
@@ -39,6 +47,7 @@ int main(void) {
         delay_ms(500);
         gpio_clear(GPIO_PIN(PORT_A, 5));
     }
+#endif
 
     return 0;
 }
