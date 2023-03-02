4c773edf95a3a9fe3b3f7e4aee1f9e8108e1a3aa
Kirill Fomin <fomin102011@yandex.ru>
2023-03-02 22:46:00 +0300

    -hwt54j-ouq3nathop

--- examples/task_miem/task_sequence/main.c
+++ examples/task_miem/task_sequence/main.c
@@ -31,15 +31,15 @@
 
 // #define BUTTON_USER (GPIO_PIN(PORT_C, 13)) // button
 
-#define BUTTON_1 (GPIO_PIN(PORT_B, 6)) // D10
-#define BUTTON_2 (GPIO_PIN(PORT_A, 7)) // D11
-#define BUTTON_3 (GPIO_PIN(PORT_A, 6)) // D12
+#define BUTTON_1 (GPIO_PIN(PORT_A, 9)) // D8
+#define BUTTON_2 (GPIO_PIN(PORT_C, 7)) // D9
+#define BUTTON_3 (GPIO_PIN(PORT_B, 6)) // D10
 
 #define LED_PIN (GPIO_PIN(PORT_A, 5)) // D13
 
-/*uint32_t currentTime(void) {
+uint32_t currentTime(void) {
     return ztimer_now(ZTIMER_USEC) / 1000;
-}*/
+}
 
 int main(void)
 {
@@ -51,35 +51,59 @@ int main(void)
     int clickCount = 0;
     int buttonSequence[4] = {0,0,0,0};
     int password[4] = {1, 2, 3, 3};
+
+    uint32_t timePush = 0;
     
     bool bPushed = false;
 
+    
+    
+
+
     while (1)
     {
-        if (clickCount < 3 && bPushed == false) {
+        //printf("b1: %i b2: %i b3: %i\n", (int)gpio_read(BUTTON_1),(int)gpio_read(BUTTON_2),(int)gpio_read(BUTTON_3));
+        if (bPushed == false)
+        {
+            timePush = currentTime();
+        }
+       // printf("%i", (unsigned long int) (timePush - currentTime()));
+        if (clickCount < 4) {
             if (gpio_read(BUTTON_1) != 0) {
-                buttonSequence[clickCount] = 1;
-                clickCount++;
                 bPushed = true;
+                if (currentTime() - timePush > 2000) {
+                    buttonSequence[clickCount] = 1;
+                    clickCount++;
+                }
+                // printf("%i", clickCount);
             } else if (gpio_read(BUTTON_2) != 0) {
-                buttonSequence[clickCount] = 2;
-                clickCount++;
                 bPushed = true;
+                if (currentTime() - timePush > 2000) {
+                    buttonSequence[clickCount] = 2;
+                    clickCount++;
+                }
+                // printf("%i", clickCount);
             } else if (gpio_read(BUTTON_3) != 0) {
-                buttonSequence[clickCount] = 3;
-                clickCount++;
                 bPushed = true;
+                if (currentTime() - timePush > 2000) {
+                    buttonSequence[clickCount] = 3;
+                    clickCount++;
+                }
+                // printf("%i", clickCount);
             } 
-        } else if (gpio_read(BUTTON_3) == 0 && gpio_read(BUTTON_3) == 0 && gpio_read(BUTTON_3) == 0) {
+        }   else if (gpio_read(BUTTON_1) == 0 && gpio_read(BUTTON_2) == 0 && gpio_read(BUTTON_3) == 0) {
             bPushed = false;
-        } else if (clickCount == 3 && buttonSequence[0] == password[0] && buttonSequence[1] == password[1]
-         && buttonSequence[2] == password[2] && buttonSequence[3] == password[3]) {
+        } else if (clickCount == 4) { 
+            if (clickCount == 3 && buttonSequence[0] == password[0] && buttonSequence[1] == password[1]
+                && buttonSequence[2] == password[2] && buttonSequence[3] == password[3]) {
+                    gpio_toggle(LED_PIN); 
+                }
             buttonSequence[0] = 0;
             buttonSequence[1] = 0;
             buttonSequence[2] = 0;
             buttonSequence[3] = 0;
             clickCount = 0;
-            gpio_toggle(LED_PIN);
+            printf("%i\n", clickCount);
         }
         // printf("led button: %i led yellow: %i\n", gpio_read(BUTTON_USER), (int)gpio_read(LED_AUTO_YELLOW));
         
